875be1254a69dacf1c0d3e085f46f23c2d2b01aa
ryq1996 <38856689+ryq506587454@users.noreply.github.com>
2020-04-03 17:04:04 +0800

    Update

--- src/com/meisi/util/UnApptSMSUtil.java
+++ src/com/meisi/util/UnApptSMSUtil.java
@@ -24,8 +24,8 @@ public class UnApptSMSUtil {
 			final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
 			final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 			//替换成你的AK
-			final String accessKeyId = "LTAINIAw7YLmGOAu";//你的accessKeyId,参考本文档步骤2
-			final String accessKeySecret = "uoCeUU83SW8y1IJupzi01PC4AjjiX0";//你的accessKeySecret，参考本文档步骤2
+			final String accessKeyId = "xxx";//你的accessKeyId,参考本文档步骤2
+			final String accessKeySecret = "xxx";//你的accessKeySecret，参考本文档步骤2
 			//初始化ascClient,暂时不支持多region（请勿修改）
 			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
 			accessKeySecret);
